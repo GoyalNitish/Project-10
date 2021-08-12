2bfe45e5cfd943cbbe95ac0fad0c456f5ac8ba64
GoyalNitish <87898669+GoyalNitish@users.noreply.github.com>
2021-08-12 16:44:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,38 @@
-var ship;
-var sea;
-function preload(){
-seaImage=loadImage('sea.png');
-shipImage=loadAnimation('ship-1.png','ship-2.png' ,'ship-3.png' , 'ship-4.png');
+var sea,ship;
+var seaImg,shipImg;
 
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-1.png",
+                            "ship-2.png","ship-1.png");
 }
 
 function setup(){
-  createCanvas(600,400);
-sea=createSprite(200,180,400,20) 
-sea.addImage("sea",seaImage);
-sea.x=sea.width/2;
-sea.velocityX= -2;
+  createCanvas(400,400);
+  background("blue");
 
-ship=createSprite(200,200,50,50);
-ship.addAnimation('mship',shipImage);
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
 
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
 }
 
-
 function draw() {
-  background("blue");
- 
- if(sea.x<0){
-   sea.x=sea.width/2;
- }
+  background(0);
+  sea.velocityX = -3;
 
-drawSprite
+  
+  //code to reset the background
+  if(sea.x < 0){
+    sea.x = sea.width/8;
   }
+    
+  drawSprites();
+}
